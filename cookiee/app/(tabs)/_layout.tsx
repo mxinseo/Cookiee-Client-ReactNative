91099e2d06c7f84734d12387ac256c5ea7ec1093
mxinseo <jade201913@gmail.com>
2024-06-01 20:32:48 +0900

    Fix : tab바 제거, tab -> stack

--- cookiee/app/(tabs)/_layout.tsx
+++ cookiee/app/(tabs)/_layout.tsx
@@ -1,37 +1,31 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs, Stack } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
+    <Stack
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-      }}>
-      <Tabs.Screen
+      }}
+    >
+      <Stack.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
+          title: "Home",
         }}
       />
-      <Tabs.Screen
+      <Stack.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
+          title: "Explore",
         }}
       />
-    </Tabs>
+    </Stack>
   );
 }
