5b2758d15fa12bd3c7e9b7abef3f64b193f22c71
mxinseo <jade201913@gmail.com>
2024-06-02 23:51:41 +0900

    Fix : 이벤트 삭제 api 수정

--- cookiee/api/event/deleteEvent.js
+++ cookiee/api/event/deleteEvent.js
@@ -9,7 +9,6 @@ export const deleteEvent = async (deviceId, eventId) => {
 
     if (res.status == 200) {
       console.log("이벤트 삭제 통신 성공. LOG의 'ok'가 true인지 확인하세요.");
-      console.log(JSON.stringify(res));
       return res.ok;
     }
   } catch (err) {
